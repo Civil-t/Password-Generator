41270d8e89f2a3433e2ecbbe858a46ed9120f47d
Tapiwa <tapiwachimbwanda4@gmail.com>
2024-01-05 12:46:02 -0500

    finished working properly no extra milestones

--- index.js
+++ index.js
@@ -1,6 +1,110 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
 
+let pass1 = document.getElementById("pass1");
+pass1.textContent = "Your Secure Password1";
+let pass2 = document.getElementById("pass2");
+pass2.textContent = "Your Secure Password2";
 
-
+function generatePassword() {
+  let password1 = "";
+  let password2 = "";
+  for (let i = 0; i < 16; i++) {
+    password1 += characters[Math.floor(Math.random() * characters.length)];
+    password2 += characters[Math.floor(Math.random() * characters.length)];
+  }
+  pass1.textContent = password1;
+  pass2.textContent = password2;
+}
 
